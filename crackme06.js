c05468adfffcea38dec23b7684a965933974f7f8
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-26 00:11:13 +0100

    done

---
+++ crackme06.js
@@ -0,0 +1,6 @@
+let password = '1a2b!AZEAZEAZEAZEAZEAZEAZ'
+const crackme6 = (password)
+if (password[0, 1, 2, 3, 4] === '1a2b!' && password.length > 13) {
+  console.log('OK')
+} else
+  console.log('BAD')
